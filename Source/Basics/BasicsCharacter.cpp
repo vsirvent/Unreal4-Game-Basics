c376ff9ac2bfd3a2a58dadbcfec4f203b8bd2a71
vsirvent <vicentesirvent@gmail.com>
2020-10-13 21:32:20 +0200

    Initial commit

--- Source/Basics/BasicsCharacter.cpp
+++ Source/Basics/BasicsCharacter.cpp
@@ -31,6 +31,7 @@ ABasicsCharacter::ABasicsCharacter()
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
+	GetCharacterMovement()->GravityScale = 2.0f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
