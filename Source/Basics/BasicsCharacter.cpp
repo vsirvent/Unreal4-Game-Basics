e11bbf81adcc63243c1b12ed7db8d2211bda4d86
vsirvent <vicentesirvent@gmail.com>
2020-10-24 10:55:46 +0200

    Testing

--- Source/Basics/BasicsCharacter.cpp
+++ Source/Basics/BasicsCharacter.cpp
@@ -36,7 +36,7 @@ ABasicsCharacter::ABasicsCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 1000.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
